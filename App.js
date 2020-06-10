4dc2f81517669d94dfe6c01fdca20ecec6a6e93f
pkj-ind <pramodix@gmail.com>
2020-06-10 22:32:32 +0530

    setting flexDirection row

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding:30}}>
-      <View>
-        <TextInput />
+    <View style={{padding:50}}>
+      <View style={{flexDirection : 'row', justifyContent:'space-between', alignContent:'center'}}>
+        <TextInput style={{borderColor: 'black', borderWidth: 1, width:'80%'}} />
         <Button title="Add"/>
       </View>
       <View>
