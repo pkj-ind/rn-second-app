bfcacbedd346d3f14df13bb5fbf409a90935f986
pkj-ind <pramodix@gmail.com>
2020-06-11 02:23:54 +0530

    stylesheet objs

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding:50}}>
-      <View style={{flexDirection : 'row', justifyContent:'space-between', alignContent:'center'}}>
-        <TextInput style={{borderColor: 'black', borderWidth: 1, width:'80%'}} />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Welcome" style={styles.input} />
         <Button title="Add"/>
       </View>
       <View
@@ -55,11 +55,14 @@ export default function App() {
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  screen: {
+    padding:50
+  },
+  inputContainer: {
+  flexDirection : 'row', 
+  justifyContent:'space-between', 
+  alignContent:'center'},
+  input:{borderColor: 'black', borderWidth: 1, width:'80%'}
+  
+});
