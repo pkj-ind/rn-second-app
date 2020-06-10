a4f7bd4c104cd057018066212bc94ce18bbb3f8f
pkj-ind <pramodix@gmail.com>
2020-06-11 01:36:37 +0530

    flexbox deep dive

--- App.js
+++ App.js
@@ -8,9 +8,49 @@ export default function App() {
         <TextInput style={{borderColor: 'black', borderWidth: 1, width:'80%'}} />
         <Button title="Add"/>
       </View>
-      <View>
-
+      <View
+      style={{
+        padding:20,
+        flexDirection: 'row',
+        width: '80%',
+        height: 300,
+        justifyContent: 'space-around',
+        alignItems: 'stretch',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          flex: 1,
+          justifyContent: 'space-evenly',
+          alignItems: 'flex-start'
+        }}
+      >
+        <Text>3</Text>
+        <Text>4</Text>
+        <Text>5</Text>
+      </View>
+    </View>
     </View>
   );
 }
