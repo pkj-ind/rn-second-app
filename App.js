073aeade5aa896c599f44fb0fc5e32d44878c588
pkj-ind <pramodix@gmail.com>
2020-06-13 09:52:57 +0530

    FlatList with random keygenerated

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
   const [enterGoal, setEnterGoal] = useState("");
@@ -8,7 +15,10 @@ export default function App() {
     setEnterGoal(enteredText);
   };
   const addGoalHandler = () => {
-    setGoalList((goalList) => [...goalList, enterGoal]);
+    setGoalList((goalList) => [
+      ...goalList,
+      { key: Math.random().toString(), value: enterGoal },
+    ]);
     setEnterGoal("");
   };
   return (
@@ -22,17 +32,18 @@ export default function App() {
         />
         <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <ScrollView>
-        {goalList.map((goal) => (
-          <View key={goal} style={styles.listItems}>
-            <Text>{goal}</Text>
+      <FlatList
+        data={goalList}
+        renderItem={(itemData) => (
+          <View style={styles.listItems}>
+            <Text>{itemData.item.value}</Text>
           </View>
-        ))}
-       </ScrollView>
+        )}
+      />
+
       <View>
         <Text>I am testing scroll option</Text>
       </View>
-     
     </View>
   );
 }
@@ -40,21 +51,21 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50,
-    marginBottom:20
+    marginBottom: 20,
   },
   inputContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignContent: "center",
-    marginBottom:10
+    marginBottom: 10,
   },
   input: { borderColor: "black", borderWidth: 1, width: "80%" },
-  listItems:{
-    padding:5,
+  listItems: {
+    padding: 5,
     borderRadius: 4,
-    backgroundColor: '#ccc',
-    borderWidth:1,
-    borderColor:"black",
-    marginVertical: 5
-  }
+    backgroundColor: "#ccc",
+    borderWidth: 1,
+    borderColor: "black",
+    marginVertical: 5,
+  },
 });
