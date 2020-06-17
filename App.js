da0a2d7fb582e81864dc3f5730b6373df1219486
pkj-ind <pramodix@gmail.com>
2020-06-17 22:58:02 +0530

    Modal with styling for taking user input

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Button } from "react-native";
 
 export default function App() {
   const [goalList, setGoalList] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   const addGoalHandler = (enterGoal, setEnterGoal) => {
     setGoalList((goalList) => [
       ...goalList,
       { id: Math.random().toString(), value: enterGoal },
     ]);
+    setShowModal(false);
     setEnterGoal("");
   };
 
-  const removeGoalHandler =(goalId) =>{
-    setGoalList((goalList)=>{
-      return goalList.filter((goal)=>goal.id !== goalId)
-    })
-  }
-  return (  
+  const removeGoalHandler = (goalId) => {
+    setGoalList((goalList) => {
+      return goalList.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  const closeModalInput = () => {
+    setShowModal(false);
+  };
+  return (
     <View style={styles.screen}>
-      <GoalInput addGoal={addGoalHandler} />
+      <Button title="Add New Goal" onPress={() => setShowModal(true)} />
+      <GoalInput
+        visible={showModal}
+        addGoal={addGoalHandler}
+        closeInputModal={closeModalInput}
+      />
 
       <FlatList
         keyExtractor={(item, index) => item.id}
@@ -30,7 +41,7 @@ export default function App() {
           <GoalItem
             id={itemData.item.id}
             onDelete={removeGoalHandler}
-            onLongPress={()=>console.log("I am long Presed")}
+            onLongPress={() => console.log("I am long Presed")}
             title={itemData.item.value}
           />
         )}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,32 +1,58 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, Modal } from "react-native";
 
 export default function GoalInput(props) {
-
   const [enterGoal, setEnterGoal] = useState("");
 
   const handleChangeText = (enteredText) => {
     setEnterGoal(enteredText);
   };
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Add Goales"
-        style={styles.input}
-        onChangeText={handleChangeText}
-        value={enterGoal}
-      />
-      <Button title="Add" onPress={props.addGoal.bind(this,enterGoal,setEnterGoal)} />
-    </View>
+    // animationType brings modal in center
+    <Modal animationType="slide" visible={props.visible}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add Goales"
+          style={styles.input}
+          onChangeText={handleChangeText}
+          value={enterGoal}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+          <Button
+            title="Add"
+            onPress={props.addGoal.bind(this, enterGoal, setEnterGoal)}
+          />
+          </View >
+          <View style={styles.button}>
+          <Button title="Cancel" color="red" onPress={props.closeInputModal} />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignContent: "center",
+    // flexDirection: "row",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
     marginBottom: 10,
   },
-  input: { borderColor: "black", borderWidth: 1, width: "80%" },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "60%",
+  },
+  button:{
+    width:'40%'
+  }
 });
